ff2dfd53faef52fa74c852c70fb1e24a173dff04
Akshay Neje <akshayneje2823@gmail.com>
2022-09-25 19:55:27 +0530

    Done

--- cart/client/cart-pro/src/components/Products/CreateProduct.jsx
+++ cart/client/cart-pro/src/components/Products/CreateProduct.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
 
 function CreateProduct() {
-  return (
-    <div>CreateProduct</div>
-  )
+    const [product, setProduct] = useState({ name: "", image: "", price: "", qty: "", info: "" })
+
+    const onchangeHandler = (event) => {
+        setProduct({ ...product, [event.target.name]: event.target.value })
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        Axios.post("http://localhost:8080/api/products", product)
+    }
+
+    return (
+        <div className='container mt-5'>
+            {console.log(product)}
+            <div className="col-md-6">
+                <div className="card">
+                    <div className="card-header">
+                        <h4>Create Product</h4>
+                    </div>
+                    <div className="card-body">
+                        <form onSubmit={submitHandler}>
+                            <div className="form-group">
+                                <input
+                                    autoFocus
+                                    type="text"
+                                    className='form-control'
+                                    placeholder='Enter Product Name...'
+                                    name='name'
+                                    onChange={onchangeHandler}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <input t
+                                    ype="number"
+                                    className='form-control'
+                                    placeholder='Enter Price...'
+                                    name='price'
+                                    onChange={onchangeHandler}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <input
+                                    type="text"
+                                    className='form-control'
+                                    placeholder='Enter QTY...'
+                                    name='qty'
+                                    onChange={onchangeHandler}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <input
+                                    type="text"
+                                    className='form-control'
+                                    placeholder='Info...'
+                                    name='info'
+                                    onChange={onchangeHandler}
+                                />
+                            </div>
+                            <button type='submit' className='btn btn-success btn-md'>Create Product</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default CreateProduct
